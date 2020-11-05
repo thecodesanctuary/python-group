8b00f32520883bcc325ca187aed298482215c57d
Yates2099 <gabrielyawe11@gmail.com>
2020-11-05 22:55:25 +0100

    commit on main

--- assessment/assessment_1/Gabriel_Yawe/Week 2/2.py
+++ assessment/assessment_1/Gabriel_Yawe/Week 2/2.py
@@ -12,7 +12,7 @@ else:
     print("Fibonacci Sequence: ")
     while count < nterms:
         print(n1)
-        nth =n1 + n2
+        nth =n1 + n2 
         n1=n2
         n2=nth
         count +=1
